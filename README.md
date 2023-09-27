d517b2dfbf6fd72d044be6240cce9830e4dab7c7
tridiamondli <tridiamond2023@gmail.com>
2023-09-27 17:19:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### forwardrequest
+> 基于python flask构建的一个简单的用于代理转发请求url，用来解决5秒盾问题
+> 利用到cloudscraper库，用于解决5秒盾问题
+
+### 使用方法
+安装flask，cloudscraper库
+```
+pip install flask
+pip install cloudscraper
+```
+请求方法
+#### GET
+GET方法传递参数
+```
+url，User-Agent，Referer，Cookie
+```
---
+++ forwardrequest.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+
+from flask import Flask, request
+import cloudscraper
+
+app = Flask(__name__)
+
+@app.route('/proxy', methods=['GET'])
+def proxy():
+
+    url = request.args.get('url')
+    UserAgent_str = request.args.get('User-Agent')
+    Referer_str = request.args.get('Referer')
+    Cookie_str = request.args.get('Cookie')
+
+    scraper = cloudscraper.create_scraper()
+
+    if UserAgent_str is not None:
+        scraper.headers.update({'User-Agent': UserAgent_str})
+    if Referer_str is not None:
+        scraper.headers.update({'Referer': Referer_str})
+    if Cookie_str is not None:
+        scraper.cookies.update({'Cookie': Cookie_str})
+
+    try:
+        response = scraper.get(url)
+        return response.text
+    except :
+        return "error"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
